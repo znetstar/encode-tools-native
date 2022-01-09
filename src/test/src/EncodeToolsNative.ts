303cf84f00b1e9216d66b32f1c44fcf080c86fd4
Zachary Boyd <zachary@zacharyboyd.nyc>
2022-01-08 20:13:14 -0500

    1.3.3

--- src/test/src/EncodeToolsNative.ts
+++ src/test/src/EncodeToolsNative.ts
@@ -210,7 +210,9 @@ describe('EncodeToolsNative', async function () {
       }
       const buf = (Buffer.from(bufArr));
       const jsonObj = enc.serializeObject({ foo: buf });
-      const obj = JSON.parse(jsonObj);
+
+      // @ts-ignore
+      const obj = JSON.parse(Buffer.from(jsonObj, 'hex'));
       assert.deepEqual(obj.foo, buf.toString('hex'));
     });
   });
